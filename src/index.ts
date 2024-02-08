e24970c9f53671b08dd91d2ab9a08cc6cfb9df01
Vladimir Arshinnikov <lordofrain7@gmail.com>
2024-02-08 14:15:24 +0200

    rafactor: cnange svg through css to svg through sprites

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,32 @@
 import './index.scss';
 
-document.addEventListener('DOMContentLoaded', () => {
-  const burgerButton = document.querySelector('.burger__button') as HTMLElement;
-  const mobileNav = document.querySelector('.mobile__nav') as HTMLElement;
-  const mobileNavLinks = document.querySelectorAll('.mobile__nav .nav__link') as NodeListOf<HTMLElement>;
+const burgerButton = document.querySelector('.burger__button') as HTMLElement;
+const mobileNav = document.querySelector('.mobile__nav') as HTMLElement;
+const mobileNavLinks = document.querySelectorAll('.mobile__nav .nav__link') as NodeListOf<HTMLElement>;
+const isBurgerButtonAndMobileNavExist = burgerButton && mobileNav;
 
-  if (burgerButton && mobileNav) {
-    burgerButton.addEventListener('click', () => {
-      mobileNav.classList.toggle('active');
-      burgerButton.classList.toggle('active');
-    });
-  }
+if (isBurgerButtonAndMobileNavExist) {
+  burgerButton.addEventListener('click', () => {
+    mobileNav.classList.toggle('active');
+    burgerButton.classList.toggle('active');
+  });
+}
 
-  mobileNavLinks.forEach((link) => {
-    link.addEventListener('click', () => {
+mobileNavLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    mobileNav.classList.remove('active');
+    burgerButton.classList.remove('active');
+  });
+});
+
+document.addEventListener('click', (event: MouseEvent) => {
+  if (isBurgerButtonAndMobileNavExist) {
+    const isClickInsideNav = mobileNav.contains(event.target as Node);
+    const isClickInsideBurgerButton = burgerButton.contains(event.target as Node);
+
+    if (!isClickInsideNav && !isClickInsideBurgerButton) {
       mobileNav.classList.remove('active');
       burgerButton.classList.remove('active');
-    });
-  });
-
-  document.addEventListener('click', (event: MouseEvent) => {
-    if (mobileNav && burgerButton) {
-      const isClickInsideNav = mobileNav.contains(event.target as Node);
-      const isClickInsideBurgerButton = burgerButton.contains(event.target as Node);
-
-      if (!isClickInsideNav && !isClickInsideBurgerButton) {
-        mobileNav.classList.remove('active');
-        burgerButton.classList.remove('active');
-      }
     }
-  });
+  }
 });
